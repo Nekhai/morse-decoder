6ad0f01beb473f43f5a4968bda99214ff8c393a5
Nekhai <NekhaiYurii@gmail.com>
2021-08-19 13:25:28 +0300

    feat: add 'Morse code decoder' task

--- src/index.js
+++ src/index.js
@@ -38,9 +38,32 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let result = '';
+  if (expr.length % 10 !== 0) {
+    result = 0;
+  } else {
+    for (let i = 0; i < expr.length; i += 10) {
+      let str = expr.slice(i, i +10);
+      if (str === '**********') result += ' ';
+      let acum = '';
+      let subStr = '';
+      for (let j = 0; j < str.length; j += 2) {
+        subStr = str[j] + str[j + 1];
+        console.log(subStr)
+        if (subStr === '10') acum += '.';
+        if (subStr === '11') acum += '-';
+        }
+      for (let key in MORSE_TABLE) {
+        if (key === acum) result += MORSE_TABLE[key];
+      }
+    }
+  }
+  console.log(result)
+  return result;
 }
 
+
+console.log (decode("00101010100000000010"))
 module.exports = {
     decode
 }
